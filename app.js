dc5d3dcf5c05097615d90e0b75c7f7daa7ed39ae
mutiu4luv <chidiemmamadu@gmail.com>
2026-02-10 14:52:02 +0100

    json mb

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const registrationsRoute = require("./routes/userRoute");
 const admin = require("./routes/admin");
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // ✅ Allow only your frontend + local dev
 const allowedOrigins = [
