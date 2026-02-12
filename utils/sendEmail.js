8a0ff74dde75d1eeafcb4b545f46573f46cb8f07
mutiu4luv <chidiemmamadu@gmail.com>
2026-02-12 08:19:52 +0100

    i removed brevo

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,31 +1,31 @@
-// utils/sendEmail.js
-const brevo = require("@getbrevo/brevo");
+// // utils/sendEmail.js
+// const brevo = require("@getbrevo/brevo");
 
-const sendEmail = async ({ email, subject, html }) => {
-  try {
-    const apiInstance = new brevo.TransactionalEmailsApi();
+// const sendEmail = async ({ email, subject, html }) => {
+//   try {
+//     const apiInstance = new brevo.TransactionalEmailsApi();
 
-    // Use your API key from .env
-    apiInstance.setApiKey(
-      brevo.TransactionalEmailsApiApiKeys.apiKey,
-      process.env.BREVO_API_KEY
-    );
+//     // Use your API key from .env
+//     apiInstance.setApiKey(
+//       brevo.TransactionalEmailsApiApiKeys.apiKey,
+//       process.env.BREVO_API_KEY
+//     );
 
-    const sendSmtpEmail = new brevo.SendSmtpEmail();
-    sendSmtpEmail.sender = {
-      name: "De-Harmelodic Chorale",
-      email: process.env.BREVO_VERIFIED_EMAIL,
-    };
-    sendSmtpEmail.to = [{ email }];
-    sendSmtpEmail.subject = subject;
-    sendSmtpEmail.htmlContent = html;
+//     const sendSmtpEmail = new brevo.SendSmtpEmail();
+//     sendSmtpEmail.sender = {
+//       name: "De-Harmelodic Chorale",
+//       email: process.env.BREVO_VERIFIED_EMAIL,
+//     };
+//     sendSmtpEmail.to = [{ email }];
+//     sendSmtpEmail.subject = subject;
+//     sendSmtpEmail.htmlContent = html;
 
-    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    return response;
-  } catch (err) {
-    console.error("Forgot Password Error (Brevo API):", err);
-    throw new Error("Failed to send email");
-  }
-};
+//     const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
+//     return response;
+//   } catch (err) {
+//     console.error("Forgot Password Error (Brevo API):", err);
+//     throw new Error("Failed to send email");
+//   }
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
